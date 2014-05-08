ad79df21bf2575e46513b4f7f9aacbf4c6216d11
Robert Carr <robert.carr@canonical.com>
2014-05-08 11:47:26 -0700

    Track button states in mir

--- mir/mir_pointer.cc
+++ mir/mir_pointer.cc
@@ -11,31 +11,44 @@ namespace om = ozonemir;
 om::Pointer::Pointer(ui::EventConverterOzoneWayland *sink, unsigned handle)
     : sink(sink),
       handle(handle) {
+    for (int i = 0; i < num_buttons; i++)
+        button_depressed[i] = false;
 }
 
 om::Pointer::~Pointer() {
 }
 
 void om::Pointer::handle_motion_event(MirMotionEvent const& mev) {
+    // TODO: Tracking the button state like this is a pain...
+    static int const num_buttons = 3;
+    static MirMotionButton buttons[3] = {mir_motion_button_primary,
+                                         mir_motion_button_secondary,
+                                         mir_motion_button_tertiary};
+    static ui::EventFlags ozone_buttons[3] = {ui::EF_LEFT_MOUSE_BUTTON,
+                                              ui::EF_RIGHT_MOUSE_BUTTON,
+                                              ui::EF_MIDDLE_MOUSE_BUTTON};
+                                    
     if (mev.action == mir_motion_action_down ||
-        mev.action == mir_motion_action_up) {
-      ui::EventFlags flags = (ui::EventFlags)0;
-      // TODO: Modifiers
-      if (mev.button_state == mir_motion_button_primary) {
-        flags = ui::EF_LEFT_MOUSE_BUTTON;
-      }
-      else if (mev.button_state == mir_motion_button_secondary) {
-        flags = ui::EF_RIGHT_MOUSE_BUTTON; // TODO: Correct? ~racarr
-      }
-      else if (mev.button_state == mir_motion_button_tertiary) {
-        flags = ui::EF_MIDDLE_MOUSE_BUTTON;
-      }
+        mev.action == mir_motion_action_up)
+    {
+        for (int i = 0; i < num_buttons; i++)
+        {
+            auto state = mev.button_state & buttons[i];
+            if (state != button_depressed[i])
+            {
+                // TODO: Modifiers?
+                ui::EventFlags flags;
+                flags = ozone_buttons[i];
 
-      ui::EventType type = mev.action == mir_motion_action_down ? ui::ET_MOUSE_PRESSED : ui::ET_MOUSE_RELEASED;
+                ui::EventType type = mev.action == mir_motion_action_down ? ui::ET_MOUSE_PRESSED : ui::ET_MOUSE_RELEASED;
 
-      sink->ButtonNotify(handle, type, 
+                sink->ButtonNotify(handle, type, 
                          flags, mev.pointer_coordinates[0].x,
                          mev.pointer_coordinates[0].y);
+
+            }
+            button_depressed[i] = state;
+        }
     }
     else if (mev.action == mir_motion_action_move) {
       sink->MotionNotify(mev.pointer_coordinates[0].x,
